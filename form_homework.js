d5c01769bde23f3424519716bbaac806a84b89c9
Molly <tina61310@gmail.com>
2026-04-16 14:19:47 +0800

    修改textarea寬度

--- form_homework.js
+++ form_homework.js
@@ -5,15 +5,15 @@ const g = document.querySelector("#green");
 const r = document.querySelector("#red");
 const colors = [y, g, r];
 
-colors.forEach((everycolor) => {
-  everycolor.addEventListener("click", (e) => {
-    clear.style.display = "block"; //按任一顏色會跑出清除按鈕
+colors.forEach((eachcolor) => {
+  eachcolor.addEventListener("click", (e) => {
+    clear.style.display = "block";
   });
 });
 
 clear.addEventListener("click", (e) => {
-  colors.forEach((everycolor) => {
-    everycolor.checked = false;
+  colors.forEach((eachcolor) => {
+    eachcolor.checked = false;
     clear.style.display = "none";
   });
 });
